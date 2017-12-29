952fc897829e41a03378a0397b2bc7abab3a24ae
Trent <trent@sadfunction.com>
2017-12-28 21:30:21 -0500

    Initial commit.

---
+++ desktop/src/com/sad/function/rogue/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sad.function.rogue.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sad.function.rogue.RogueLike;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RogueLike(), config);
+	}
+}
