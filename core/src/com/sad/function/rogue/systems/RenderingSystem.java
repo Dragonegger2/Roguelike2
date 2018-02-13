bce3e15adaeb829f848e0310d13df41fd8921b22
Trent Lennon <tlennon@EBSCO.COM>
2018-02-13 08:37:59 -0500

    Not sure what I changed.

--- core/src/com/sad/function/rogue/systems/RenderingSystem.java
+++ core/src/com/sad/function/rogue/systems/RenderingSystem.java
@@ -14,6 +14,7 @@ import com.sad.function.rogue.components.SpriteComponent;
 import java.util.UUID;
 
 public class RenderingSystem {
+    private static final float WORLD_TO_BOX = 1/16f;
 
     public RenderingSystem() {
         init();
@@ -28,11 +29,14 @@ public class RenderingSystem {
         for(UUID entity : em.getAllEntitiesPossessingComponents(new Class[] { SpriteComponent.class, PhysicsComponent.class })) {
             //get the PhysicsComponent once. Maybe it saves a bunch of lookups, who knows? I probably should.
             PhysicsComponent tmp = em.getComponent(entity, PhysicsComponent.class);
-            batch.draw(em.getComponent(entity, SpriteComponent.class).sprite,
-                    tmp.body.getPosition().x - 0.5f ,
-                    tmp.body.getPosition().y - 0.5f,
-                    1,
-                    1);
+            SpriteComponent spriteTemp = em.getComponent(entity, SpriteComponent.class);
+
+            //Convert all units from world, to the Box2D units.
+            batch.draw(spriteTemp.sprite,
+                    tmp.body.getPosition().x - (spriteTemp.sprite.getWidth() * WORLD_TO_BOX / 2),  //DON'T FORGET TO FIX OFFSET, We're using lower-left as the origin and box2d uses the center as an origin.
+                    tmp.body.getPosition().y - (spriteTemp.sprite.getHeight() * WORLD_TO_BOX / 2), //DON'T FORGET TO FIX OFFSET, We're using lower-left as the origin and box2d uses the center as an origin.
+                    spriteTemp.sprite.getWidth() * WORLD_TO_BOX,
+                    spriteTemp.sprite.getHeight() * WORLD_TO_BOX);
 
         }
 
